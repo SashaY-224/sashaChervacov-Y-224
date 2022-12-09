8179c397c02d702a469f792d166f3b62494a2c22
Sasha Chervacov <114074792+SashaY-224@users.noreply.github.com>
2022-12-09 15:17:00 +0300

    Delete Задание 1.py

--- Практическая работа №12/Блок А/Задание 1.py
+++
@@ -1,8 +0,0 @@
-print("№1")
-def f(A,B):
-    if A==0 and B == 0:
-        return 1
-    else:
-        for i in range(A, B-1, -1): print(i)
-        for i in range(A, B+1): print(i)
-print(f(14,18))
