f7cebc52cac6367d8097f3dbf95886a79c3d2475
Sasha Chervacov <114074792+SashaY-224@users.noreply.github.com>
2022-12-09 15:31:05 +0300

    Update задание 8.py

--- Практическая работа №4/задание 8/задание 8.py
+++ Практическая работа №4/задание 8/задание 8.py
@@ -1,8 +1,10 @@
+def f(n):
+    s = ''
+    for i in range(1,n+1):
+        s= s+str(i)
+    print(s)
 n = int(input())
-for i in range(n):
-    for s in range(1, i+2):
-        print(s, end='')
-    print()
+f(n)
 
             
     
